af252acedb05db073669967240a3927096fb23b9
Ronak2001 <Ronak.01@outlook.com>
2021-05-11 16:52:12 -0400

    Day-1: Hello world - spring application

---
+++ src/test/java/com/ronak/projectSpring/learningspring/LearningSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ronak.projectSpring.learningspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
